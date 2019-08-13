c0badb7dd9cf2594e7ef113172c26e9a0d0c1162
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2019-08-13 17:31:04 +0200

    get back default bow as image

--- src/components/Items/ItemViewer.jsx
+++ src/components/Items/ItemViewer.jsx
@@ -36,7 +36,7 @@ class ItemViewer extends React.Component {
                     label: 'Name',
                     field: 'Name',
                     sort: 'asc',
-                    width: 150
+                    width: 100
                 },
                 {
                     label: 'Stats',
@@ -47,7 +47,7 @@ class ItemViewer extends React.Component {
                     label: 'Level Required',
                     field: 'LevelRequired',
                     sort: 'asc',
-                    width: 50
+                    width: 10
                 }
             ]
         };
@@ -109,11 +109,12 @@ class ItemViewer extends React.Component {
 
                     </div>
                 </>;
-                var imageUrl = window.location.origin.toString() + '/' + item.ImageName + '.gif';
+                //var imageUrl = window.location.origin.toString() + '/' + item.ImageName + '.gif';
+                var defaultImageUrl = window.location.origin.toString() + '/longbattlebow.gif';
                 console.log(self);
                 var itemName = <>
                     {item.Name}
-                    <img className="item-image border info rounded mb-0" src={imageUrl}  onError={self.SetDefaultImageOnError}  alt="testImage.." />
+                    <img className="item-image border info rounded mb-0" src={defaultImageUrl}  onError={self.SetDefaultImageOnError}  alt="testImage.." />
                 </>;
 
                 return {
