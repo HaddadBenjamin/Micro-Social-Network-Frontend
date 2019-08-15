eceade6775eb30675675c263d701d58271b509cd
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2019-08-15 09:36:07 +0200

    hide previous search

--- src/shared/components/Search.tsx
+++ src/shared/components/Search.tsx
@@ -19,7 +19,7 @@ class Search extends Component<Props, State>
             <>
                 <MDBCol md="12" className="form-inline d-flex justify-content-end">
                     <label>Search
-                    <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" />
+                    <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search (Work in progress)" aria-label="Search" />
                     </label>
                 </MDBCol>
                 <div className="py-2"></div>
