616f3f3e8e02bf161d31368da6ec6cd53d69c177
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2020-03-29 11:52:26 +0200

    search refactoring

--- src/shared/components/Search.tsx
+++ src/shared/components/Search.tsx
@@ -1,61 +1,51 @@
 import React, {Component} from "react";
-import {MDBCol } from "mdbreact";
-import {filter } from 'lodash'
+import {MDBCol} from "mdbreact";
+import {filter} from 'lodash'
 import Item from "../../components/Items/Item";
+import "./Search.css"
 
-export interface SearchResult
-{
-    elements : Item[],
-    searchedTerm : string,
+export interface SearchResult<T> {
+    elements: Item[],
+    searchedTerm: string,
 }
 
-interface Props
-{
-    elements : Item[],
-    onSearch : (elements : SearchResult) => void,
-    searchFilter: (Element : any, searchTerm : string) => boolean
+interface Props<T> {
+    elements: Item[],
+    onSearch: (elements: SearchResult<T>) => void,
+    searchFilter: (Element: any, searchTerm: string) => boolean
 }
 
-class Search extends Component<Props, {}>
-{
-    constructor(props : Props)
-    {
-        super(props);
-
-        this.onSearch = this.onSearch.bind(this);
-    }
-
-    onSearch(event : any)
-    {
-        this.props.onSearch({
+const Search = (props: Props<any>) => {
+    function onSearch(event: any) {
+        props.onSearch({
             searchedTerm: event.target.value,
-            elements: filter(this.props.elements, (element) => { return this.props.searchFilter(element, event.target.value) })
+            elements: filter(props.elements, (element) => {
+                return props.searchFilter(element, event.target.value)
+            })
         });
     }
 
-    render()
-    {
-        return (
-            <>
-                <MDBCol size="4" className="form-inline d-flex justify-content-end fa-pull-right">
-                    <input
-                           style={{
-                               "fontFamily" : "Diablo",
-                               "fontSize": "15.4px",
-                                "color" : "white",
-                                "boxShadow" : "2px 2px 4px 0.1px #eb0d26",
-                                "backgroundColor" : "rgba(1, 1, 1, 0.8)",
-                                "border" : "0.05em solid white"}}
-                           className="form-control form-control-sm ml-3 w-75 shared-searchs py-3"
-                           type="text"
-                           placeholder="Search"
-                           aria-label="Search"
-                           onChange={this.onSearch} />
-                </MDBCol>
-                <div className="py-4"></div>
-            </>
-        )
-    }
+    return (
+        <>
+            <MDBCol size="4" className="form-inline d-flex justify-content-end fa-pull-right">
+                <input
+                    style={{
+                        "fontFamily": "Diablo",
+                        "fontSize": "15.4px",
+                        "color": "white",
+                        "boxShadow": "2px 2px 4px 0.1px #eb0d26",
+                        "backgroundColor": "rgba(1, 1, 1, 0.8)",
+                        "border": "0.05em solid white"
+                    }}
+                    className="form-control form-control-sm ml-3 w-75 shared-searchs py-3"
+                    type="text"
+                    placeholder="Search"
+                    aria-label="Search"
+                    onChange={onSearch}/>
+            </MDBCol>
+            <div className="py-4"></div>
+        </>
+    )
 }
 
 export default Search;
