9532b918c0566f07f12a9afb97c84e380a6f7b8a
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2020-03-29 12:05:18 +0200

    move css

--- src/shared/components/Search.tsx
+++ src/shared/components/Search.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import {MDBCol} from "mdbreact";
 import {filter} from 'lodash'
 import Item from "../../components/Items/Item";
@@ -29,14 +29,6 @@ const Search = (props: Props<any>) => {
         <>
             <MDBCol size="4" className="form-inline d-flex justify-content-end fa-pull-right">
                 <input
-                    style={{
-                        "fontFamily": "Diablo",
-                        "fontSize": "15.4px",
-                        "color": "white",
-                        "boxShadow": "2px 2px 4px 0.1px #eb0d26",
-                        "backgroundColor": "rgba(1, 1, 1, 0.8)",
-                        "border": "0.05em solid white"
-                    }}
                     className="form-control form-control-sm ml-3 w-75 shared-searchs py-3"
                     type="text"
                     placeholder="Search"
