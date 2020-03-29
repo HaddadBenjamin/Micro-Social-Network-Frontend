b6052d75c216a9d827f4d9182ab94fb444290f82
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2020-03-29 13:18:55 +0200

    type item reducer

--- src/reducers/item.reducer.ts
+++ src/reducers/item.reducer.ts
@@ -2,27 +2,24 @@ import Item from "../components/Items/Item";
 
 export interface ItemState
 {
-    items : Item[]
+    items: Item[]
 }
 
-const initialState =
+const initialState: ItemState =
 {
-    items : []
+    items: []
 };
-export const searchItemsReducer = (state : ItemState = initialState, action : any) =>
+export const searchItemsReducer = (itemState: ItemState = initialState, action: string) =>
 {
-    console.log(...initialState.items || [])
-    console.log(action.payload)
-    switch (action.type)
-    {
+    switch (action.type) {
         case "SEARCH_ITEMS":
             return {
-                ...initialState.items || [],
-                items : action.payload || []
+                ...initialState.items,
+                items: action.payload || []
             };
 
         default:
-            return state;
+            return itemState;
     }
 };
 
--- src/shared/utilities/strings.ts
+++ src/shared/utilities/strings.ts
@@ -3,7 +3,7 @@ class Strings
     public toTitleCase(text: string)
     {
         const FirstCharacterIndex : number = 0;
-        const FromCharacterIndex : number = 0;
+        const FromCharacterIndex : number = 1;
 
         return text.replace(
             /\w\S*/g,
